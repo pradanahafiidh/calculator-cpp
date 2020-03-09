50353db20aa9d60c870ea0399ca8ab9604a15cde
pradanahafiidh <pradanahafidh@gmail.com>
2020-03-09 17:05:13 +0700

    calculator

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,11 @@ class calculator
 {
 	static void Main(String[]args) 
 	{
+		int a;
+		int b;
 		
+		Console.WriteLine("Hasil Penambahan",a,b, Penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan",a,b, Pengurangan(a,b));
 	}
 	static int Penambahan (int a, int b)
 	{
