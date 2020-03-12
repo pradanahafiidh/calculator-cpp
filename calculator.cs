ca9cec5b4169cd1145cb3c9f321d9a88d0badccc
pradanahafiidh <pradanahafidh@gmail.com>
2020-03-12 23:06:01 +0700

    menambahkan fungsi perkalian dan pembagian

--- calculator.cs
+++ calculator.cs
@@ -1,21 +1,29 @@
-class calculator 
+using System;
+
+public class calculator 
 {
-	static void Main(String[]args) 
-	{
-		int a;
-		int b;
-		
-		Console.WriteLine("Hasil Penambahan",a,b, Penambahan(a,b));
-		Console.WriteLine("Hasil Pengurangan",a,b, Pengurangan(a,b));
-	}
-	static int Penambahan (int a, int b)
-	{
-		return a + b;
-	}
-	static int Pengurangan (int a , int b)
-	{
-		return a - b;
-	}
+    static void Main(String[]args) 
+    {
+        int a;
+        int b;
+        
+        Console.WriteLine("Hasil Penambahan",a,b, Penambahan(a,b));
+        Console.WriteLine("Hasil Pengurangan",a,b, Pengurangan(a,b));
+    }
+    static int Penambahan (int a, int b)
+    {
+        return a + b;
+    }
+    static int Pengurangan (int a , int b)
+    {
+        return a - b;
+    }
+     static int perkalian(int a, int b)
+    {
+        return a * b;
+    }
+    static int pembagian(int a, int b)
+    {
+        return a / b;
+    }
 }
-	
-	
